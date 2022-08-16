b4ace40da242a65519ca0dc3a28ffcebdb74d273
BADA LEE <80818640+lbd0@users.noreply.github.com>
2022-08-16 16:18:02 +0900

    주석 추가

--- mole_test/app/src/main/java/com/lbd0/MainActivity.java
+++ mole_test/app/src/main/java/com/lbd0/MainActivity.java
@@ -3,6 +3,7 @@ package com.lbd0;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -23,17 +24,17 @@ import javax.xml.transform.Result;
 public class MainActivity extends AppCompatActivity {
 
 
-    TextView timer;
-    TextView score;
-    TextView bestScore;
-    Button start;
+    TextView timer;     // 타이머 텍스트
+    TextView score;     // 점수 텍스트
+    TextView bestScore; // 최고점수 텍스트
+    Button start;       // 시작 버튼
 
-    ImageView[] img_array = new ImageView[9];
+    ImageView[] img_array = new ImageView[9];   // 두더지 이미지 배열
     int[] imageID = {R.id.img1, R.id.img2, R.id.img3, R.id.img4, R.id.img5, R.id.img6, R.id.img7, R.id.img8, R.id.img9};
 
-    final String TAG_ON = "on";
-    final String TAG_OFF = "off";
-    int scores = 0;
+    final String TAG_ON = "on";     // 두더지가 올라옴
+    final String TAG_OFF = "off";   // 두더지가 내려감
+    int scores = 0;                 // 점수
 
     Thread thread = null;
 
@@ -51,40 +52,40 @@ public class MainActivity extends AppCompatActivity {
             bestScore.setText("Best Score : " + scores);
         } else {
             bestScore.setText("Best Score : " + ResultActivity.spf.getInt("spfscore",0));
-        }
+        }       // 최고 점수 표시
 
         for(int i=0; i<img_array.length; i++) {
             img_array[i] = findViewById(imageID[i]);
-            img_array[i].setImageResource(R.drawable.off);
-            img_array[i].setTag(TAG_OFF);
+            img_array[i].setImageResource(R.drawable.off);  // 두더지 들어가 있는 사진
+            img_array[i].setTag(TAG_OFF);                   // 두더지 들어감
 
-            img_array[i].setOnClickListener(new View.OnClickListener(){
+            img_array[i].setOnClickListener(new View.OnClickListener(){     // 그림 클릭 했는가
                 public void onClick(View v) {
-                    if(((ImageView)v).getTag().toString().equals(TAG_ON)) {
-                        Toast.makeText(getApplicationContext(), "good", Toast.LENGTH_LONG).show();
-                        score.setText(String.valueOf(scores++));
-                        ((ImageView)v).setImageResource(R.drawable.off);
-                        v.setTag(TAG_OFF);
+                    if(((ImageView)v).getTag().toString().equals(TAG_ON)) {     // 두더지가 올라와 있을 때 그림 클릭 했으면
+                        Toast.makeText(getApplicationContext(), "good", Toast.LENGTH_SHORT).show();
+                        score.setText(String.valueOf(scores++));    // 점수++
+                        ((ImageView)v).setImageResource(R.drawable.off);    // 두더지 들어간 이미지로 변경
+                        v.setTag(TAG_OFF);      // 들어감 태그로 변경
                     }else {
-                        Toast.makeText(getApplicationContext(), "bad", Toast.LENGTH_LONG).show();
+                        Toast.makeText(getApplicationContext(), "bad", Toast.LENGTH_SHORT).show();
                         if(scores<=0) {
                             scores = 0;
                             score.setText(String.valueOf(scores));
                         }else {
-                            score.setText(String.valueOf(scores--));
+                            score.setText(String.valueOf(scores--));    // 점수--
                         }
 
                         ((ImageView)v).setImageResource(R.drawable.animal);
-                        v.setTag(TAG_ON);
+                        v.setTag(TAG_ON);       // 나옴 태그로 변경
                     }
                 }
             });
         }
 
-        timer.setText("Timer 30sec");
-        score.setText("0");
+        timer.setText("Timer 30sec");   // 타이머 텍스트 세팅
+        score.setText("0");             // 점수 텍스트 세팅
 
-        start.setOnClickListener(new View.OnClickListener(){
+        start.setOnClickListener(new View.OnClickListener(){        // 시작 버튼 누르면 실행
             public void onClick(View v) {
 
                 start.setVisibility(View.GONE);
@@ -100,22 +101,24 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    Handler onHandler = new Handler() {
+    @SuppressLint("HandlerLeak")
+    Handler onHandler = new Handler() {         // 나옴 핸들러
 
         public void handleMessage(Message msg) {
-            img_array[msg.arg1].setImageResource(R.drawable.animal);
-            img_array[msg.arg1].setTag(TAG_ON);
+            img_array[msg.arg1].setImageResource(R.drawable.animal);    // 두더지가 나온 이미지로 설정
+            img_array[msg.arg1].setTag(TAG_ON);                         // 나옴 태그로 설정
         }
     };
 
-    Handler offHandler = new Handler() {
+    @SuppressLint("HandlerLeak")
+    Handler offHandler = new Handler() {        // 들어감 핸들러
         public void handleMessage(Message msg) {
-            img_array[msg.arg1].setImageResource(R.drawable.off);
-            img_array[msg.arg1].setTag(TAG_OFF);
+            img_array[msg.arg1].setImageResource(R.drawable.off);      // 두더지가 들어간 이미지로 설정
+            img_array[msg.arg1].setTag(TAG_OFF);                       // 들어감 태그로 설정
         }
     };
 
-    public class DThread implements Runnable {
+    public class DThread implements Runnable {  // 코틀린으로 할 때는 코루틴을 써야하나
         int index = 0;
 
         DThread(int index) {
@@ -126,13 +129,13 @@ public class MainActivity extends AppCompatActivity {
             while(true) {
                 try{
                     Message msg1 = new Message();
-                    int offtime = new Random().nextInt(5000) + 500;
+                    int offtime = new Random().nextInt(5000) + 500;     // 들어간 시간
                     Thread.sleep(offtime);
 
                     msg1.arg1 = index;
                     onHandler.sendMessage(msg1);
 
-                    int ontime = new Random().nextInt(1000) + 500;
+                    int ontime = new Random().nextInt(1000) + 500;      // 나온 시간
                     Thread.sleep(ontime);
                     Message msg2 = new Message();
                     msg2.arg1 = index;
@@ -144,33 +147,34 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    Handler handler = new Handler() {
+    @SuppressLint("HandlerLeak")
+    Handler handler = new Handler() {       // 타이머 텍스트 설정 핸들러
         public void handleMessage(Message msg) {
             timer.setText("Timer " + msg.arg1 + "sec");
         }
     };
 
-    public class timeCheck implements Runnable{
+    public class timeCheck implements Runnable{     // 타이머 클래스
 
-        final int MAXTIME = 30;
+        final int MAXTIME = 30; // 제한 시간
 
-        public void run() {
+        public void run() {     // 시간 점점 줄어듦
             for(int i=MAXTIME; i >= 0; i--) {
                 Message msg = new Message();
                 msg.arg1 = i;
                 handler.sendMessage(msg);
 
-                try{
+                try{        // 예외처리?
                     Thread.sleep(1000);
                 }catch(InterruptedException e) {
                     e.printStackTrace();
                 }
             }
 
-            Intent intent = new Intent(MainActivity.this, ResultActivity.class);
-            intent.putExtra("score" , scores);
-            startActivity(intent);
-            finish();
+            Intent intent = new Intent(MainActivity.this, ResultActivity.class);    // 인텐트
+            intent.putExtra("score" , scores);      // 점수 보내
+            startActivity(intent);  // ResultActivity 실행
+            finish();   // 종료
         }
     }
 }
--- mole_test/app/src/main/java/com/lbd0/ResultActivity.java
+++ mole_test/app/src/main/java/com/lbd0/ResultActivity.java
@@ -13,7 +13,7 @@ public class ResultActivity extends AppCompatActivity {
 
     TextView sub_result;
     Button sub_retry;
-    public static SharedPreferences spf = null;
+    public static SharedPreferences spf = null;     // 최고 점수 저장
 
     protected void onCreate(Bundle saveInstanceState) {
         super.onCreate(saveInstanceState);
@@ -24,21 +24,25 @@ public class ResultActivity extends AppCompatActivity {
         sub_result = findViewById(R.id.sub_result);
         sub_retry = findViewById(R.id.sub_retry);
 
-        int score = getIntent().getIntExtra("score", -1);
-        sub_result.setText(String.valueOf(score));
+        int score = getIntent().getIntExtra("score", -1);   // 점수
+        sub_result.setText(String.valueOf(score));      // 텍스트뷰로 점수 표시
 
-        if(spf.getInt("spfscore",0) < score) {
-            spf.edit().putInt("spfscore", score).commit();
-            sub_result.setText("New Best\n" + score);
-        } else {
-            sub_result.setText("Score\n" + score);
+        if(spf == null) {   // null이면 최고 점수 0으로 세팅 근데 이거 예외 처리로 안 되나?
+            spf.edit().putInt("spfscore", 0).commit();
         }
 
-        sub_retry.setOnClickListener(new View.OnClickListener(){
+        if(spf.getInt("spfscore",0) < score) {  // 최고 점수가 이번 점수보다 작으면 
+            spf.edit().putInt("spfscore", score).commit();  // 이번 점수를 최고 점수로 올림
+            sub_result.setText("New Best\n" + score);       // 텍스트 수정
+        } else {    
+            sub_result.setText("Score\n" + score);          // 텍스트 수정
+        }
+
+        sub_retry.setOnClickListener(new View.OnClickListener(){    // 다시하기 버튼 
             public void onClick(View v) {
-                Intent intent = new Intent(ResultActivity.this, MainActivity.class);
-                startActivity(intent);
-                finish();
+                Intent intent = new Intent(ResultActivity.this, MainActivity.class);    
+                startActivity(intent);      // MainActivitiy 실행
+                finish();       // 종료
             }
         });
     }
